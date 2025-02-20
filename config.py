abee8346fa7a56a36e5ddaf130dba89ae9e9514c
Devpareek8 <165389457+Devpareek8@users.noreply.github.com>
2025-02-20 14:48:37 +0530

    Update config.py

--- config.py
+++ config.py
@@ -82,9 +82,7 @@ FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "ʜᴇʟʟᴏ {first}\n\n<b>ᴊ�
 #set your Custom Caption here, "True"
 CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", """Dev<b><a href="https://t.me/+f3iO8EMm5U0wMWZl">{file_name}</a>
  
-sɪᴢᴇ : {file_size}
-
- Jᴏɪɴ <a href="https://t.me/+f3iO8EMm5U0wMWZl">{ᴜᴘᴅᴀᴛᴇ ᴄʜᴀɴɴᴇʟ]</a></b>""")
+sɪᴢᴇ : {file_size}""")
 
 #set True if you want to prevent users from forwarding files from bot
 PROTECT_CONTENT = True if os.environ.get('PROTECT_CONTENT', "False") == "True" else False
